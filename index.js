9dfe31229a308caa6756ff37efca86c867fd2824
Eli Doran <eli@elidoran.com>
2021-04-30 22:18:20 -0400

    make a variety of changes while retaining same functionality.
    1. added some local scopes around short lived variables.
    2. handled join() in the main function and included the decimal
       value into it so we produce the final string all at once.
    3. simplified how it calculates where to start adding separators.
    4. some format tweaks like switching a while-loop to a for-loop.

--- index.js
+++ index.js
@@ -4,104 +4,102 @@
 // can specify either a Number or a String.
 function commaNumber(inputNumber, optionalSeparator, optionalDecimalChar) {
 
-  // we'll strip off and hold the decimal value to reattach later.
-  // we'll hold both the `number` value and `stringNumber` value.
-  let number, stringNumber, decimal
-
-  // default `separator` is a comma
-  const separator = optionalSeparator || ','
-
   // default `decimalChar` is a period
   const decimalChar = optionalDecimalChar || '.'
 
-  switch (typeof inputNumber) {
+  let stringNumber // we assign this in the switch block and need it later.
 
-    case 'string':
+  {
+    let number // we assign this in the switch block and need it right after.
 
-      // if there aren't enough digits to need separators then return it
-      // NOTE: some numbers which are too small will get passed this
-      //       when they have decimal values which make them too long here.
-      //       but, the number value check after this switch will catch it.
-      if (inputNumber.length < (inputNumber[0] === '-' ? 5 : 4)) {
-        return inputNumber
-      }
+    switch (typeof inputNumber) {
 
-      // remember it as a string in `stringNumber` and convert to a Number
-      stringNumber = inputNumber
+      case 'string':
 
-      // if they're not using the Node standard decimal char then replace it
-      // before converting.
-      number = Number(
-        (decimalChar !== '.') ? stringNumber.replace(decimalChar, '.') : stringNumber
-      )
-      break
+        // if there aren't enough digits to need separators then return it
+        // NOTE: some numbers which are too small will get passed this
+        //       when they have decimal values which make them too long here.
+        //       but, the number value check after this switch will catch it.
+        if (inputNumber.length < (inputNumber[0] === '-' ? 5 : 4)) {
+          return inputNumber
+        }
 
-    // convert to a string.
-    // NOTE: don't check if the number is too small before converting
-    //       because we'll need to return `stringNumber` anyway.
-    case 'number':
-      stringNumber = String(inputNumber)
-      number       = inputNumber
-      // create the string version with the decimalChar they specified.
-      // this matches what the above case 'string' produces,
-      // and, fixes the bug *not* doing this caused.
-      if ('.' !== decimalChar && !Number.isInteger(inputNumber)) {
-        stringNumber = stringNumber.replace('.', decimalChar)
-      }
-      break
+        // remember it as a string in `stringNumber` and convert to a Number
+        stringNumber = inputNumber
 
-    // return invalid type as-is
-    default: return inputNumber
+        // if they're not using the Node standard decimal char then replace it
+        // before converting.
+        number = Number(
+          (decimalChar !== '.') ? stringNumber.replace(decimalChar, '.') : stringNumber
+        )
+        break
+
+      // convert to a string.
+      // NOTE: don't check if the number is too small before converting
+      //       because we'll need to return `stringNumber` anyway.
+      case 'number':
+        stringNumber = String(inputNumber)
+        number       = inputNumber
+        // create the string version with the decimalChar they specified.
+        // this matches what the above case 'string' produces,
+        // and, fixes the bug *not* doing this caused.
+        if ('.' !== decimalChar && !Number.isInteger(inputNumber)) {
+          stringNumber = stringNumber.replace('.', decimalChar)
+        }
+        break
+
+      // return invalid type as-is
+      default: return inputNumber
+    }
+
+    // when it doesn't need a separator or isn't a number then return it
+    if ((-1000 < number && number < 1000) || isNaN(number) || !isFinite(number)) {
+      return stringNumber
+    }
   }
 
-  // when it doesn't need a separator or isn't a number then return it
-  if ((-1000 < number && number < 1000) || isNaN(number) || !isFinite(number)) {
-    return stringNumber
+  {
+    // strip off decimal value to add back in later
+    const decimalIndex = stringNumber.lastIndexOf(decimalChar)
+    let decimal
+    if (decimalIndex > -1) {
+      decimal = stringNumber.slice(decimalIndex)
+      stringNumber = stringNumber.slice(0, decimalIndex)
+    }
+
+    // finally, parse the string. Note, default 'separator' is a comma.
+    const parts = parse(stringNumber, optionalSeparator || ',')
+
+    // if there's a decimal value then add it to the parts.
+    if (decimal) {// NOTE: we sliced() it off including the decimalChar
+      parts.push(decimal)
+    }
+
+    // combine all parts for the final string (note, has separators).
+    return parts.join('')
   }
-
-  // strip off decimal value to append to the final result at the bottom
-  let decimalIndex = stringNumber.lastIndexOf(decimalChar)
-
-  if (decimalIndex > -1) {
-    decimal = stringNumber.slice(decimalIndex)
-    stringNumber = stringNumber.slice(0, decimalIndex)
-  }
-
-  // else {
-  //   decimal = null
-  // }
-
-  // finally, parse the string and add in separators
-  stringNumber = parse(stringNumber, separator)
-
-  // if there's a decimal value add it back on the end.
-  // NOTE: we sliced() it off including the decimalChar, so it's good.
-  return decimal ? stringNumber + decimal : stringNumber
-
 }
 
+function parse(string, separator) {
 
-function parse(stringNumber, separator) {
+  // find first index to split the string at (where 1st separator goes).
+  let i = ((string.length - 1) % 3) + 1
 
-  // below here we split the number at spots to add a separator.
-  // then, combine it with the separator and add decimal value (if exists)
+  // above calculation is wrong when num is negative and a certain size.
+  if (i === 1 && (string[0] === '-')) {
+    i = 4  // example: -123,456,789  start at 4, not 1.
+  }
 
-  const start = stringNumber[0] === '-' ? 1 : 0  // start after minus sign
-  const count = stringNumber.length - start - 1  // count digits after first
-  let i = (count % 3) + 1 + start                // index for first separator
-  const strings = [                              // hold string parts
-    // grab string content before where the first separator belongs
-    stringNumber.slice(0, i)
+  const strings = [ // holds the string parts
+    string.slice(0, i) // grab part before the first separator
   ]
 
   // split remaining string in groups of 3 where a separator belongs
-  while (i < stringNumber.length) {
-    strings.push(stringNumber.substr(i, 3))
-    i += 3
+  for (; i < string.length; i += 3) {
+    strings.push(separator, string.substr(i, 3))
   }
 
-  // finally, combine groups with the separator
-  return strings.join(separator)
+  return strings
 }
 
 
